5bcceb4e4378ab94a279c7e9bc79f0926b95dd1c
bullbesh <babulehaha@gmail.com>
2021-08-23 14:28:10 +0300

    Use formatter at main.py

--- main.py
+++ main.py
@@ -24,44 +24,44 @@ async def send_welcome(message):
         "/delete - удалить своё id из базы",
     )
 
+
 @dp.message_handler(commands=["add"])
 async def add_id(message):
-	connect = sqlite3.connect("users.db")
-	cursor = connect.cursor()
+    connect = sqlite3.connect("users.db")
+    cursor = connect.cursor()
 
-	cursor.execute("""CREATE TABLE IF NOT EXISTS login_id(
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS login_id(
 		id INTEGER
-	)""")
+	)"""
+    )
 
-	connect.commit()
+    connect.commit()
 
-	people_id = message.chat.id
-	cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
-	data = cursor.fetchone()
+    people_id = message.chat.id
+    cursor.execute(f"SELECT id FROM login_id WHERE id = {people_id}")
+    data = cursor.fetchone()
 
-	if data is None:
-		user_id = [message.chat.id]
-		cursor.execute("INSERT INTO login_id VALUES(?);", user_id)
-		connect.commit()
+    if data is None:
+        user_id = [message.chat.id]
+        cursor.execute("INSERT INTO login_id VALUES(?);", user_id)
+        connect.commit()
 
-		await message.answer("Ваше id успешно добавлено!")
-	else:
-		await message.answer(
-			"Такое id уже существует!"
-		)
+        await message.answer("Ваше id успешно добавлено!")
+    else:
+        await message.answer("Такое id уже существует!")
+
 
 @dp.message_handler(commands=["delete"])
 async def delete_id(message):
-	connect = sqlite3.connect("users.db")
-	cursor = connect.cursor()
+    connect = sqlite3.connect("users.db")
+    cursor = connect.cursor()
 
-	people_id = message.chat.id
-	cursor.execute(f"DELETE FROM login_id WHERE id = {people_id}")
-	connect.commit()
+    people_id = message.chat.id
+    cursor.execute(f"DELETE FROM login_id WHERE id = {people_id}")
+    connect.commit()
 
-	await message.answer(
-		"Ваше id успешно удалено!"
-	)
+    await message.answer("Ваше id успешно удалено!")
 
 
 if __name__ == "__main__":
